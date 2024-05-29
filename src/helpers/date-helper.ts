7c3dec06f5f60aae05426c0b3bc25c5fd9041f14
massimilianowosz <massimilianowosz@gmail.com>
2024-05-29 14:54:25 +0200

    Update date-helper.ts

--- src/helpers/date-helper.ts
+++ src/helpers/date-helper.ts
@@ -94,8 +94,8 @@ export const ganttDateRange = (
     case ViewMode.QuarterYear:
       newStartDate = addToDate(newStartDate, -3, "month");
       newStartDate = startOfDate(newStartDate, "month");
-      newEndDate = addToDate(newEndDate, 12, "month");
-      newEndDate = startOfDate(newEndDate, "month");
+      newEndDate = addToDate(newEndDate, 3, "year");
+      newEndDate = startOfDate(newEndDate, "year");
       break;
     case ViewMode.Month:
       newStartDate = addToDate(newStartDate, -1 * preStepsCount, "month");
